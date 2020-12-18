0fc2eb46f352fb3231e0ccb233bed266b4e2d0b7
jose MG <leo.alberto44@gmail.com>
2020-12-18 17:52:59 -0500

    MusicaExterna

--- app/src/main/java/com/example/proyectoidnp/view/reproductor/reproductor.java
+++ app/src/main/java/com/example/proyectoidnp/view/reproductor/reproductor.java
@@ -8,9 +8,16 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -26,7 +33,9 @@ import com.example.proyectoidnp.view.estadisticas.estadisticas;
 import com.example.proyectoidnp.view.historial.historial;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 
 public class reproductor extends AppCompatActivity {
     RecyclerView recyclerView;
@@ -38,14 +47,17 @@ public class reproductor extends AppCompatActivity {
     Button playPlause;
     ImageView imagenPresentada;
     TextView nombrePresentado;
+    List<AudioModel> nuevo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reproductor);
+
+        final List<AudioModel> mlista = getAllAudioFromDevice(this);
+        nuevo = mlista;
         lista = new ArrayList<reproductorModelo>();
         LinearLayoutManager linear =new LinearLayoutManager(this);
-        agregar();
-        agregarMusicas();
+
         recyclerView=findViewById(R.id.listaDatos);
         adaptador=new Adaptador(lista);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -61,6 +73,8 @@ public class reproductor extends AppCompatActivity {
         });
         imagenPresentada = (ImageView) findViewById(R.id.imageButton);
         nombrePresentado = (TextView) findViewById(R.id.mostrarNombreCancion);
+        agregar();
+        agregarMusicas();
 
         bottomNavigationView = findViewById(R.id.NavigationButton);
         bottomNavigationView.setSelectedItemId(R.id.action_musica);
@@ -96,16 +110,18 @@ public class reproductor extends AppCompatActivity {
         adaptador.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 detenerMusicas();
                 reproductorModelo datoSelec = lista.get(recyclerView.getChildAdapterPosition(view));
-                reiniciarReproductor();
-                cambioMusica(datoSelec);
-                posicion = datoSelec.getCancion();
-                Toast.makeText(getApplicationContext(),datoSelec.getNombre(),
-                        Toast.LENGTH_SHORT).show();
+                if(datoSelec.getCancion()<4) {
+                    reiniciarReproductor();
+                    cambioMusica(datoSelec);
+                    posicion = datoSelec.getCancion();
+                    Toast.makeText(getApplicationContext(), datoSelec.getNombre(),
+                            Toast.LENGTH_SHORT).show();
+                }
             }
         });
-
     }
     public void agregarMusicas(){
         vectormp[0] = MediaPlayer.create(this,R.raw.chiptronical);
@@ -121,6 +137,9 @@ public class reproductor extends AppCompatActivity {
         lista.add(new reproductorModelo("luchando",R.drawable.cancion,2));
         lista.add(new reproductorModelo("lyonesse",R.drawable.cancion,3));
         lista.add(new reproductorModelo("solvepuzzle",R.drawable.cancion,4));
+        for(int i=0 ; i<nuevo.size();i++){
+            lista.add(new reproductorModelo(nuevo.get(i).getaName(),R.drawable.cancion,i+5));
+        }
     }
     public void playPause(View view){
         if(posicion!=-1){
@@ -150,6 +169,39 @@ public class reproductor extends AppCompatActivity {
                 vectormp[i].stop();
         }
         playPlause.setBackgroundResource(R.drawable.ic_baseline_play_circle_outline_24);
+    }
+    public List<AudioModel> getAllAudioFromDevice(final Context context) {
+        final List<AudioModel> tempAudioList = new ArrayList<>();
+
+        Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        String[] projection = {MediaStore.Audio.AudioColumns.DATA, MediaStore.Audio.AudioColumns.TITLE, MediaStore.Audio.AudioColumns.ALBUM, MediaStore.Audio.ArtistColumns.ARTIST,};
+        Cursor c = context.getContentResolver().query(uri,
+                projection,
+                null,
+                null,
+                null);
+        if (c != null) {
+            while (c.moveToNext()) {
+                AudioModel audioModel = new AudioModel();
+                String path = c.getString(0);
+                String name = c.getString(1);
+                String album = c.getString(2);
+                String artist = c.getString(3);
+
+                audioModel.setaName(name);
+                audioModel.setaAlbum(album);
+                audioModel.setaArtist(artist);
+                audioModel.setaPath(path);
+
+                Log.e("Name :" + name, " Album :" + album);
+                Log.e("Path :" + path, " Artist :" + artist);
+
+                tempAudioList.add(audioModel);
+            }
+            c.close();
+        }
+
+        return tempAudioList;
     }
 
 }
